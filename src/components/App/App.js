011c65c62c12b691580f294739d0d5e0566ff1e4
Grigory Griko <griko1996@gmail.com>
2023-04-21 21:11:39 +0300

    feat: закончена верстка секции aboutProject

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
 
-import Header from '../Header/Header.js';
-import Main from '../Main/Main.js';
-import Movies from '../Movies/Movies.js';
-import SavedMovies from '../SavedMovies/SavedMovies.js';
-import Profile from '../Profile/Profile.js';
-import Login from '../Login/Login.js';
-import Register from '../Register/Register.js';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
 
 function App() {
   return (
