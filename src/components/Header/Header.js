a8de523abcb800ae71df82076defa30908f9f438
Grigory Griko <griko1996@gmail.com>
2023-04-24 16:29:43 +0300

    feat: переделана верстка компонента Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,55 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,  useLocation } from 'react-router-dom';
 
 function Header() {
+  const location = useLocation();
+
   return (
-    <header className="header section">
+    <header className={`header section ${location.pathname !== '/' ? 'header_light-theme' : ''}`}>
       <div className="header__container-content wrapper">
         <Link to="/" className="logo"></Link>
 
-        <nav className="header__wrapper-auth">
-            <Link to="signup" className="header__action-reigister">Регистрация</Link>
+      {location.pathname !== '/' ? (
+        <>
+        <nav className="header__wrapper-menu">
+          <Link to="movies" className="header__menu-link">Фильмы</Link>
 
-            <Link to="signin" className="header__action-login">Войти</Link>
+          <Link to="saved-movies" className="header__menu-link">Сохранённые фильмы</Link>
         </nav>
+        
+        <Link to="profile" className="header__action-account">Аккаунт</Link>
+        </>
+        
+      ) : (
+        <nav className="header__wrapper-menu header__wrapper-menu_unlogged">
+          <Link to="signup" className="header__action-reigister">Регистрация</Link>
+
+          <Link to="signin" className="header__action-login">Войти</Link>
+        </nav>
+      )}
       </div>
     </header>
   )
 }
 
 export default Header;
+/*
+<nav className="header__wrapper-menu">
+          <Link to="signup" className="header__action-reigister">Регистрация</Link>
+
+          <Link to="signin" className="header__action-login">Войти</Link>
+        </nav>
+        */
+
+/*
+<Link to="/" className="logo"></Link>
+
+        <nav className="header__wrapper-menu">
+          <Link to="movies" className="header__menu-link">Фильмы</Link>
+
+          <Link to="saved-movies" className="header__menu-link">Сохранённые фильмы</Link>
+        </nav>
+
+        <Link to="profile" className="header__action-account">Аккаунт</Link>
+
+        */
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Movies() {
+  return (
+    <main className="content">
+      123
+    </main>
+  )
+}
+
+export default Movies;
