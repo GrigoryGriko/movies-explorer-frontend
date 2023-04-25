2c0094007c3c6effb09cf21ee5cbe3273b7e8e97
Grigory Griko <griko1996@gmail.com>
2023-04-25 11:44:35 +0300

    feat: почти закончена верстка секции search

---
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function SearchForm() {
+  return(
+    <section className="search-form section">
+      <div className="search-form__wrapper wrapper">
+        <form className="search-form__search-input">
+          <div className="search-form_search-icon"></div>
+          <input className="search-form_input-type" placeholder="Фильм"></input>
+          <button className="search-form_search-button"></button>
+
+          <div className="search-form_stroke-line"></div>
+
+          <button className="search-form_filter-switch">
+            <div className="search-form_switch-marker">
+            </div>
+          </button>
+          <p className="search-form_filter-name">
+            Короткометражки
+          </p>
+        </form>
+
+        <div className="search-form_stroke-line-bottom"></div>
+      </div>
+    </section>
+  )
+}
+
+export default SearchForm;
