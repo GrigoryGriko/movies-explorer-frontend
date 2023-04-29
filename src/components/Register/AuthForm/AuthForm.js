a0b880be1905fd3c18413922aebe92a5d7bef657
Grigory Griko <griko1996@gmail.com>
2023-04-29 16:24:20 +0300

    feat: настроены поля input

--- src/components/Register/AuthForm/AuthForm.js
+++ src/components/Register/AuthForm/AuthForm.js
@@ -4,23 +4,35 @@ function AuthForm() {
   return (
   <section className="auth-form">
     <form className="auth-form__wrapper">
-      <p className="auth-form__caption">
-        Имя
-      </p>
-      <input className="auth-form__input"></input>
-      <div className="auth-form__stroke-line"></div>
+      <label className="auth-form__field">
+        <p className="auth-form__caption">
+          Имя
+        </p>
+        <input className="auth-form__input"></input>
+        <div className="auth-form__stroke-line"></div>
 
-      <p className="auth-form__caption">
-        E-mail
-      </p>
-      <input className="auth-form__input"></input>
-      <div className="auth-form__stroke-line"></div>
+        <span className="auth-form__input-error" style={{ display: 'none' }}>Что-то пошло не так...</span>
+      </label>
 
-      <p className="auth-form__caption">
-        Пароль
-      </p>
-      <input className="auth-form__input"></input>
-      <div className="auth-form__stroke-line"></div>
+      <label className="auth-form__field">
+        <p className="auth-form__caption">
+          E-mail
+        </p>
+        <input className="auth-form__input"></input>
+        <div className="auth-form__stroke-line"></div>
+
+        <span className="auth-form__input-error" style={{ display: 'none' }}>Что-то пошло не так...</span>
+      </label>
+
+      <label className="auth-form__field">
+        <p className="auth-form__caption">
+          Пароль
+        </p>
+        <input className="auth-form__input auth-form__input-error-data"></input>
+        <div className="auth-form__stroke-line"></div>
+
+        <span className="auth-form__input-error">Что-то пошло не так...</span>
+      </label>
     </form>
   </section>
   )
