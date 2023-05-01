d8fd08994e5c65fb92a03b5a2ac08dce08551f11
Grigory Griko <griko1996@gmail.com>
2023-05-01 14:29:09 +0300

    feat: настроен маркер ссылок в выпадающем меню

--- src/components/Main/NavTab/NavTab.js
+++ src/components/Main/NavTab/NavTab.js
@@ -17,12 +17,46 @@ function NavTab() {
           <Link to="/profile" className="header__action-account link-hover header__wrapper-menu_site-visibility">Аккаунт</Link>
         
           <div className="popup-menu">
-            <nav className="header__wrapper-menu">
-              <Link to="/" className={`header__menu-link link-hover ${location.pathname === '/' ? 'header__menu-link_underline' : ''}`}>Главная</Link>
-              
-              <Link to="/movies" className={`header__menu-link link-hover ${location.pathname === '/movies' ? 'header__menu-link_underline' : ''}`}>Фильмы</Link>
+            <button className="popup-menu__button-close"></button>
 
-              <Link to="/saved-movies" className={`header__menu-link link-hover ${location.pathname === '/saved-movies' ? 'header__menu-link_underline' : ''}`}>Сохранённые фильмы</Link>
+            <nav className="header__wrapper-menu">
+              <ul className="header__wrapper-menu-list">
+                <li className="header__wrapper-menu-item">
+                  <Link 
+                    to="/" 
+                    className="header__menu-link link-hover">
+                      Главная
+                  </Link>
+                  <div className={`header__page-marker 
+                      ${location.pathname === '/' ?
+                      'header__page-marker_visibility' : ''}`}>
+                  </div>
+                </li><br></br>
+                
+                <li className="header__wrapper-menu-item">
+                  <Link 
+                    to="/movies" 
+                    className="header__menu-link link-hover">
+                      Фильмы
+                  </Link>
+                  <div className={`header__page-marker 
+                      ${location.pathname === '/movies' ?
+                      'header__page-marker_visibility' : ''}`}>
+                  </div>
+                </li><br></br>
+
+                <li className="header__wrapper-menu-item">
+                  <Link
+                    to="/saved-movies"
+                    className="header__menu-link link-hover">
+                      Сохранённые фильмы
+                  </Link>
+                  <div className={`header__page-marker 
+                      ${location.pathname === '/saved-movies' ?
+                      'header__page-marker_visibility' : ''}`}>
+                  </div>
+                </li>
+              </ul>
             </nav>
             
             <Link to="/profile" className="header__action-account link-hover">Аккаунт</Link>
